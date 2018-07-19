d311f4f701c4af79f25c965a41ebbee409973cf9
afeef bsharat <afeef200229@hotmail.com>
2018-07-19 14:48:57 +0300

    The best website ever

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask ,render_template
+import random
+
+app = Flask(__name__)
+
+@app.route("/")
+def load_page():
+	return render_template("home.jinja")
+
+@app.route('/<page_name>/')
+def go_to_page(page_name):
+	hobbies = ["karate","basket", "watch_anime"]
+	hobby_list = random.sample(hobbies , 2)
+	return render_template(page_name + ".jinja", hobby_list = hobby_list ,title=page_name)
+
+
+
+# @app.route("/about/")
+# def load_page_2():
+# 	return render_template("about.html")
+
+# @app.route("/contact/")
+# def load_page_3():
+# 	return render_template("contact.html")
+
+# @app.route("/hobbies/")
+# def load_page_4():
+# 	return render_template("hobbies.html")
+
+
+
+if __name__ == '__main__':
+	app.run()
