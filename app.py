dba4f671b32f57e0b039fefb11455ff09ca2808e
afeef bsharat <afeef200229@hotmail.com>
2018-07-19 18:49:35 +0300

    4

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask ,render_template
+from flask import Flask ,render_template , request
 import random
 
 app = Flask(__name__)
@@ -11,21 +11,25 @@ def load_page():
 def go_to_page(page_name):
 	# hobbies = ["karate","basket", "watch_anime"]
 	# hobby_list = random.sample(hobbies , 2)
+
 	return render_template(page_name + ".jinja" ,title = page_name)
 
+@app.route("/formsubmit", methods = ["GET" , "POST"])
+def formExmaple():
+	return "Thank You "
 
 
-# @app.route("/about/")
-# def load_page_2():
-# 	return render_template("about.html")
+# @app.route("/formsubmite", methods=["GET", "POST"])
+# def formExmaple():
+#   if request.method == "GET":
+#     render_template("login.jinja")
+#   else:
+#     form = request.form
+#     contentValue = form["content"]
+#     return contentValue
 
-# @app.route("/contact/")
-# def load_page_3():
-# 	return render_template("contact.html")
 
-# @app.route("/hobbies/")
-# def load_page_4():
-# 	return render_template("hobbies.html")
+
 
 
 
